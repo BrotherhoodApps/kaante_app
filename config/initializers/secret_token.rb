3e32e904f26b608b45a2595ccf604fe7f6c6b330
BrotherhoodApps <contactus@brotherhoodapps.co.uk>
2011-04-17 15:34:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaanteApp::Application.config.secret_token = 'd70939918b46ed73f13e8b7dac96e55ef641016e563152e4a4279f538c4f5fc95d66f69b71fed0c28cd617738a752306d1ddb2df45d4da098af58e244ff83074'
